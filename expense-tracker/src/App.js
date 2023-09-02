fcb7fc47d5f87e0d2b20832027ff0ca5ef28d500
26shubhapriya <shubhapriya1998@gmail.com>
2023-09-02 17:19:17 +0530

    creating sign up screen

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+   <>
+   <Signup />
+   </>
+  );
+}
+
+export default App;
